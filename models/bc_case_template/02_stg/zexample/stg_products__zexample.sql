64b1d91209f77cebfd3acbd4cd16785dab8d5844
russell.magidson@randstadusa.com <russell.magidson@randstadusa.com>
2023-02-03 08:59:47 -0500

    refactor folder structure

--- models/bc_case_template/02_stg/zexample/stg_products__zexample.sql
+++
@@ -1,9 +0,0 @@
-{{ config(
-    schema="stg_zexample",
-    alias='stg_products',
-    materialized='table',
-    tags = ["zexample"], 
-    enabled = false
-    ) }}
-
-SELECT * FROM {{ source('name_source_atlanta', 'products') }}
