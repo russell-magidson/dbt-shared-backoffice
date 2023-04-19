3d2ff813664c94eb5241ecc552f2b3ce04fb225a
russell-magidson <88747432+russell-magidson@users.noreply.github.com>
2023-04-19 08:47:02 -0400

    Remove unused sources

--- models/04_dm/BI_PO_Assignments/dm_fact_active_po_assignments.sql
+++
@@ -1,8 +0,0 @@
-{{ config(
-    tags = ["fact_active_po_assignments", "po_assignments"], 
-    alias = "fact_active_po_assignments"
-    )
-}}
-
-select *, current_timestamp() AS insert_datetime
-from {{ ref( 'stg_fact_active_po_assignments')}}
--- models/04_dm/BI_PO_Assignments/dm_vw_fact_active_po_assignments.sql
+++
@@ -1,9 +0,0 @@
-{{ config(
-    tags = ["vw_fact_active_po_assignments", "view", "po_assignments"], 
-    alias = "vw_fact_active_po_assignments", 
-    materialized = 'view'
-    )
-}}
-
-select *
-from {{ ref( 'dm_fact_active_po_assignments')}}
